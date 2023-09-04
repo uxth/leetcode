502e3ce61e884d248e34e9752430ff8db7396759
Ning Xu <ning.xu@tusimple.ai>
2023-09-04 10:34:46 -0700

    724

--- cpp/724.find-pivot-index.cpp
+++ cpp/724.find-pivot-index.cpp
@@ -58,13 +58,23 @@
 // @lc code=start
 class Solution {
 public:
+    // int pivotIndex(vector<int>& nums) {
+    //     int sum = accumulate(nums.begin(), nums.end(), 0);
+    //     int acc = 0;
+    //     for(int i=0;i<nums.size(); ++i)
+    //     {
+    //         if(acc * 2 == sum-nums[i]) return i;
+    //         acc += nums[i];
+    //     }
+    //     return -1;
+    // }
     int pivotIndex(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        int acc = 0;
-        for(int i=0;i<nums.size(); ++i)
-        {
-            if(acc * 2 == sum-nums[i]) return i;
-            acc += nums[i];
+        int right = accumulate(nums.begin(), nums.end(), 0);
+        int left = 0;
+        for(int i=0;i<nums.size(); ++i){
+            right -= nums[i];
+            if(left == right) return i;
+            left += nums[i];
         }
         return -1;
     }
