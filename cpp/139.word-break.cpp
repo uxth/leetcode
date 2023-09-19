07b30a3910d3eb290939331f5875031dc4c1e220
twtypsj <twtypsj@gmail.com>
2023-09-19 08:36:36 -0700

    139

--- cpp/139.word-break.cpp
+++ cpp/139.word-break.cpp
@@ -7,11 +7,11 @@
 // @lc code=start
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
+    bool bfs(string& s, vector<string>& wordDict) {
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
         queue<string> q;
         q.push(s);
-        while(!s.empty()){
+        while(!q.empty()){
             string f = q.front(); q.pop();
             if(dict.count(f)) return true;
             for(int i=1; i<f.size(); ++i){
@@ -23,6 +23,27 @@ public:
         }
         return false;
     }
+    bool dp(string& s, vector<string>& wordDict){
+        unordered_set<string> dict(begin(wordDict),end(wordDict));
+        vector<bool> dp(s.size() + 1);
+        dp[0] = true;
+        for(int i=1; i<=s.size(); ++i){
+            for(int j=i-1; j>=0; --j){
+                string sub = s.substr(j, i-j);
+                if(dict.count(sub) && dp[j]){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp.back();
+    }
+    bool wordBreak(string s, vector<string>& wordDict){
+        return dp(s, wordDict);
+    }
 };
 // @lc code=end
-
+/*
+"catsandog"\n["cats","dog","sand","and","cat"]
+"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"\n["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+*/
