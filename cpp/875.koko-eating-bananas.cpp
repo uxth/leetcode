e092c379f3b88e3a4de761f203b9d1798d8eeea4
twtypsj <twtypsj@gmail.com>
2023-11-08 08:31:07 -0800

    875

--- cpp/875.koko-eating-bananas.cpp
+++ cpp/875.koko-eating-bananas.cpp
@@ -15,19 +15,19 @@
  *
  * Koko loves to eat bananas.  There are N piles of bananas, the i-th pile has
  * piles[i] bananas.  The guards have gone and will come back in H hours.
- * 
+ *
  * Koko can decide her bananas-per-hour eating speed of K.  Each hour, she
  * chooses some pile of bananas, and eats K bananas from that pile.  If the
  * pile has less than K bananas, she eats all of them instead, and won't eat
  * any more bananas during this hour.
- * 
+ *
  * Koko likes to eat slowly, but still wants to finish eating all the bananas
  * before the guards come back.
- * 
+ *
  * Return the minimum integer K such that she can eat all the bananas within H
  * hours.
- * 
- * 
+ *
+ *
  * Example 1:
  * Input: piles = [3,6,7,11], H = 8
  * Output: 4
@@ -37,35 +37,51 @@
  * Example 3:
  * Input: piles = [30,11,23,4,20], H = 6
  * Output: 23
- * 
- * 
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 1 <= piles.length <= 10^4
  * piles.length <= H <= 10^9
  * 1 <= piles[i] <= 10^9
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int minEatingSpeed(vector<int>& piles, int H) {
-        int left = 1; int right = *max_element(piles.begin(), piles.end());
-        while(left<right)
+    int minEatingSpeed(vector<int> &piles, int H)
+    {
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+        int res = -1;
+        while (left <= right)
         {
             int mid = (left + right) / 2;
-            int timeNeeded = 0;
-            for(int i : piles) timeNeeded += ceil((float)i/mid);
+            long long timeNeeded = 0;
+            for (int i : piles)
+                timeNeeded += ceil((double)i / mid);
 
-            if(timeNeeded > H) left = mid + 1;
-            else right = mid;
+            if (timeNeeded > H)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+                res = mid;
+            }
         }
-        return left;
+        return res;
     }
 };
 // @lc code=end
 // [30,11,23,4,20]\n5
 // [30,11,23,4,20]\n6
+/*
+[1000000000]\n2
+[805306368,805306368,805306368]\n1000000000
+*/
