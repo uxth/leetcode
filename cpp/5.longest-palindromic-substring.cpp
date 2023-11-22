fc5f1020c10802484fb991cfed4d8c0271f4b4b5
twtypsj <twtypsj@gmail.com>
2023-11-22 10:27:37 -0800

    5

---
+++ cpp/5.longest-palindromic-substring.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=5 lang=cpp
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (31.74%)
+ * Likes:    28047
+ * Dislikes: 1662
+ * Total Accepted:    2.8M
+ * Total Submissions: 8.3M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        
+        int maxLen = 1;
+        std::string maxStr = s.substr(0, 1);
+        s = "#" + std::regex_replace(s, std::regex(""), "#") + "#";
+        std::vector<int> dp(s.length(), 0);
+        int center = 0;
+        int right = 0;
+        
+        for (int i = 0; i < s.length(); ++i) {
+            if (i < right) {
+                dp[i] = std::min(right - i, dp[2 * center - i]);
+            }
+            
+            while (i - dp[i] - 1 >= 0 && i + dp[i] + 1 < s.length() && s[i - dp[i] - 1] == s[i + dp[i] + 1]) {
+                dp[i]++;
+            }
+            
+            if (i + dp[i] > right) {
+                center = i;
+                right = i + dp[i];
+            }
+            
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                maxStr = s.substr(i - dp[i], 2 * dp[i] + 1);
+                maxStr.erase(std::remove(maxStr.begin(), maxStr.end(), '#'), maxStr.end());
+            }
+        }
+        
+        return maxStr; 
+    }
+};
+// @lc code=end
+
