4953624822112bb87098c52cac58d2cf10cc3e15
twtypsj <twtypsj@gmail.com>
2023-11-29 15:42:36 -0800

    249

--- cpp/249.group-shifted-strings.cpp
+++ cpp/249.group-shifted-strings.cpp
@@ -51,7 +51,6 @@ public:
                 if (d < 'a') d += 26;
                 t[i] = d;
             }
-            cout << t << endl;
             mp[t].push_back(s);
         }
         vector<vector<string>> ans;
