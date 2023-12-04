a38e18126a0868f2caf3ac6c93899421df5224cf
twtypsj <twtypsj@gmail.com>
2023-12-03 20:48:50 -0800

    319

--- cpp/319.bulb-switcher.cpp
+++ cpp/319.bulb-switcher.cpp
@@ -38,10 +38,13 @@
 // @lc code=start
 class Solution {
 public:
+    // int bulbSwitch(int n) {
+    //     int counts = 0;
+    //     for (int i=1; i*i<=n; ++i)++counts; 
+    //     return counts;
+    // }
     int bulbSwitch(int n) {
-        int counts = 0;
-        for (int i=1; i*i<=n; ++i)++counts; 
-        return counts;
+        return sqrt(n);
     }
 };
 // @lc code=end
